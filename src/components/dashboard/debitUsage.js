c33a47dd7712d6f53dcd76d53e95d2736344738b
Valentin BURGAUD <valentin.burgaud@sii.fr>
2019-11-16 18:38:47 +0100

    Modofication de l'affichage du débit

--- src/components/dashboard/debitUsage.js
+++ src/components/dashboard/debitUsage.js
@@ -3,9 +3,8 @@ import Paper from "material-ui/Paper";
 import { ResponsiveContainer } from "recharts";
 import GlobalStyles from "../../styles";
 import {typography} from "material-ui/styles";
-import {amber600, amber500, white} from "material-ui/styles/colors";
-import ArrowUp from "material-ui/svg-icons/navigation/arrow-drop-up";
-import ArrowDown from "material-ui/svg-icons/navigation/arrow-drop-down";
+import {amber600, amber500, white, black} from "material-ui/styles/colors";
+import Arrow from "material-ui/svg-icons/navigation/arrow-forward";
 import InfoBox from "./InfoBox";
 
 const debitUsage = props => {
@@ -13,49 +12,52 @@ const debitUsage = props => {
     paper: {
       minHeight: 344,
       color: white,
-      backgroundColor: amber500,
+      backgroundColor: white,
     },
     legend: {
       paddingTop: 20
     },
     pieChartDiv: {
-      height: 290,
       textAlign: "center"
     },
     header: {
       fontSize: 24,
       fontWeight: typography.fontWeightLight,
-      color: white,
-      backgroundColor: amber600,
+      color: black,
+      backgroundColor: white,
       padding: 10
     },
     iconSpan: {
-      float: "left",
+      // float: "left",
       height: 90,
       width: 90,
       textAlign: "center",
-      backgroundColor: amber600
+      backgroundColor: white
     },
     icon: {
       height: 48,
       width: 48,
       marginTop: 20,
       maxWidth: "100%"
-    }
+    },
+    affichageDebit: {
+      paddingTop: '2em',
+    },
   };
 
   return (
     <Paper style={styles.paper}>
-      <div style={{ ...styles.header }}>Utilisation du débit</div>
+      <div style={styles.header}>Utilisation du débit</div>
 
       <div style={GlobalStyles.clear} />
 
       <div className="row">
-        <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+        <div className="col-xs-12 col-sm-8 col-md-8 col-lg-6" style={styles.affichageDebit}>
           <div style={styles.pieChartDiv}>
             <ResponsiveContainer>
               <InfoBox
-                Icon={ArrowUp}
+                Icon={Arrow}
+                debit={"montant"}
                 color={amber600}
                 title="Débit montant"
                 value="124 MB/S"
@@ -64,11 +66,12 @@ const debitUsage = props => {
           </div>
         </div>
 
-        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-6" style={styles.affichageDebit}>
           <div style={styles.pieChartDiv}>
             <ResponsiveContainer>
               <InfoBox
-                Icon={ArrowDown}
+                Icon={Arrow}
+                debit={"descendant"}
                 color={amber600}
                 title="Débit descendant"
                 value="30 MB/S"
